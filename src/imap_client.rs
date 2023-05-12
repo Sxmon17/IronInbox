c74ecb6fc1965f25ebbd467ca5753651e4695bf5
Sxmon17 <simonguglberger@gmail.com>
2023-05-12 12:41:41 +0200

    fixed: imap_client and smtp_cient are now public

--- src/imap_client.rs
+++ src/imap_client.rs
@@ -1,33 +1,33 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-struct ImapClient {
+pub struct ImapClient {
     stream: TcpStream,
 }
 
 impl ImapClient {
     /// Create a new IMAP client connected to the given server.
-    fn new(server: &str, username: &str, password: &str) -> std::io::Result<Self> {
+    pub fn new(server: &str, username: &str, password: &str) -> std::io::Result<Self> {
         let mut stream = TcpStream::connect(server)?;
         Self::send_command(&mut stream, "LOGIN", &format!("{} {}", username, password))?;
         Ok(Self { stream })
     }
 
     /// Send a command to the server.
-    fn send_command(stream: &mut TcpStream, command: &str, args: &str) -> std::io::Result<()> {
+    pub fn send_command(stream: &mut TcpStream, command: &str, args: &str) -> std::io::Result<()> {
         stream.write_all(format!("{} {}\r\n", command, args).as_bytes())?;
         stream.flush()
     }
 
     /// Read the response from the server.
-    fn read_response(stream: &mut TcpStream) -> std::io::Result<String> {
+    pub fn read_response(stream: &mut TcpStream) -> std::io::Result<String> {
         let mut response = String::new();
         stream.read_to_string(&mut response)?;
         Ok(response)
     }
 
     /// Receive emails.
-    fn receive_mail(&mut self) -> std::io::Result<Vec<String>> {
+    pub fn receive_mail(&mut self) -> std::io::Result<Vec<String>> {
         Self::send_command(&mut self.stream, "SELECT", "INBOX")?;
         Self::send_command(&mut self.stream, "FETCH", "1:* (BODY[TEXT])")?;
         let response = Self::read_response(&mut self.stream)?;
@@ -38,7 +38,7 @@ impl ImapClient {
         Ok(mails)
     }
 
-    fn logout(&mut self) -> std::io::Result<()> {
+    pub fn logout(&mut self) -> std::io::Result<()> {
         Self::send_command(&mut self.stream, "LOGOUT", "")
     }
 }
--- src/smtp_client.rs
+++ src/smtp_client.rs
@@ -6,17 +6,20 @@ pub struct SmtpClient {
 }
 
 impl SmtpClient {
-    fn new(server: &str) -> std::io::Result<Self> {
+    /// Create a new SMTP client connected to the given server.
+    pub fn new(server: &str) -> std::io::Result<Self> {
         let stream = TcpStream::connect(server)?;
         Ok(Self { stream })
     }
 
-    fn send_command(&mut self, cmd: &str) -> std::io::Result<()> {
+    /// Send a command to the server.
+    pub fn send_command(&mut self, cmd: &str) -> std::io::Result<()> {
         self.stream.write_all(cmd.as_bytes())?;
         self.stream.flush()
     }
 
-    fn send_mail(&mut self, from: &str, to: &str, subject: &str, body: &str) -> std::io::Result<()> {
+    /// Send an email.
+    pub fn send_mail(&mut self, from: &str, to: &str, subject: &str, body: &str) -> std::io::Result<()> {
         self.send_command(&format!("MAIL FROM:<{}>\r\n", from))?;
         self.send_command(&format!("RCPT TO:<{}>\r\n", to))?;
         self.send_command("DATA\r\n")?;
@@ -26,7 +29,8 @@ impl SmtpClient {
         self.send_command(".\r\n")
     }
 
-    fn quit(&mut self) -> std::io::Result<()> {
+    /// Quit the session.
+    pub fn quit(&mut self) -> std::io::Result<()> {
         self.send_command("QUIT\r\n")
     }
 }
