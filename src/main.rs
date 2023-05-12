4a969664bcb3afa215edaa9bb5bf04f7eb5de31b
Sxmon17 <simonguglberger@gmail.com>
2023-05-12 12:44:04 +0200

    implemented: logic for receiving mails in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+mod smtp_client;
+mod imap_client;
+mod utils;
+mod cli;
+
+use clap::{Clap, Command};
+use smtp_client::SmtpClient;
+use imap_client::ImapClient;
+use utils::{print_success, print_error};
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    match opts.cmd {
+        Some(Command::Send(send)) => {
+            // ... SMTP code here ...
+        }
+        Some(Command::Receive) => {
+            match ImapClient::new(&opts.imap_server, &opts.username, &opts.password) {
+                Ok(mut client) => {
+                    match client.receive_mail() {
+                        Ok(mails) => {
+                            for (i, mail) in mails.iter().enumerate() {
+                                println!("Mail {}: {}", i + 1, mail.green());
+                            }
+                        }
+                        Err(e) => {
+                            print_error(&format!("Failed to receive mail: {}", e));
+                        }
+                    }
+                    if let Err(e) = client.logout() {
+                        print_error(&format!("Failed to logout: {}", e));
+                    }
+                }
+                Err(e) => {
+                    print_error(&format!("Failed to connect to IMAP server: {}", e));
+                }
+            }
+        }
+        _ => {}
+    }
 }
